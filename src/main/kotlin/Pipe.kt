a0d0402ead189515e76b9e80a499196a08d8720f
Shilin Wu <shwu@confluent.io>
2023-05-17 13:09:07 +0800

    Initial

--- src/main/kotlin/Pipe.kt
+++ src/main/kotlin/Pipe.kt
@@ -118,7 +118,6 @@ data class Pipe(
         if((isSrc && data.srcHasData())||(!isSrc && data.destHasData())) {
             // can't read when data is on hold
             myKey.interestOps(myKey.interestOps() and SelectionKey.OP_READ.inv())
-            otherKey.interestOps(otherKey.interestOps() or SelectionKey.OP_WRITE)
             // can't read
             return
         }
@@ -168,7 +167,6 @@ data class Pipe(
         if((isSrc && !!data.destHasData())||(isDest && !!data.srcHasData())) {
             // can't read when data is on hold
             myKey.interestOps(myKey.interestOps() and SelectionKey.OP_WRITE.inv())
-            otherKey.interestOps(otherKey.interestOps() or SelectionKey.OP_READ)
             // can't read
             return
         }
